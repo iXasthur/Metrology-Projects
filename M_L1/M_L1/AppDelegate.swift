6738454c9970466665f73d4056ad67b89b2ecfad
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2019-09-25 19:47:20 +0300

    +L1 proj

---
+++ M_L1/M_L1/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  M_L1
+//
+//  Created by Михаил Ковалевский on 25/09/2019.
+//  Copyright © 2019 Mikhail Kavaleuski. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+        print("-> Terminating app")
+    }
+
+
+}
+
---
+++ M_L1/M_L1/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  M_L1
+//
+//  Created by Михаил Ковалевский on 25/09/2019.
+//  Copyright © 2019 Mikhail Kavaleuski. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var source: String = ""
+        
+        do {
+            source = try String(contentsOf: Bundle.main.url(forResource: "src", withExtension: "scala")!)
+        } catch let err {
+            fatalError(err.localizedDescription)
+        }
+        
+        print(source)
+        
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
