15f02fd78c5672959272d6fa0e4cd6b280def5a6
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2019-09-27 11:40:00 +0300

    + L1 Console project

---
+++ M_L1_TERMINAL/M_L1_TERMINAL/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  M_L1_TERMINAL
+//
+//  Created by Михаил Ковалевский on 27/09/2019.
+//  Copyright © 2019 Mikhail Kavaleuski. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+
+
+
